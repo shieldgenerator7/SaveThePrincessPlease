b2391128cb14aeb3bf7d637395fdbcde93624b33
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-12-11 16:31:24 -0500

    Added DisplayUpdater

---
+++ Assets/Scripts/DisplayUpdater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayUpdater : MonoBehaviour {
+
+    private SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        updateDisplayOrder();
+        //If this object is stationary,
+        if (GetComponent<Rigidbody2D>() == null)
+        {
+            //Remove this script after setting the display order
+            Destroy(this);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateDisplayOrder();
+    }
+
+    void updateDisplayOrder()
+    {
+        sr.sortingOrder = -(int)(transform.position.y * 10);
+    }
+}
