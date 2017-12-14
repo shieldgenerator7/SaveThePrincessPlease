0760f09a0b7efa4dcf2ef52923dee6a43f561f51
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-12-13 19:47:14 -0500

    Bandaged missing reference exception
    This bug was caused by a Skeleton registering a method for a delegate,
    getting deleted, and then having that delegate try to call the method on
    that destroyed object.

--- Assets/Scripts/SkeletonAI.cs
+++ Assets/Scripts/SkeletonAI.cs
@@ -36,6 +36,9 @@ public class SkeletonAI : MonoBehaviour {
 
     void refocus()
     {
-        controller.processTapGesture(initialTarget.transform.position, initialTarget);
+        if (controller)
+        {
+            controller.processTapGesture(initialTarget.transform.position, initialTarget);
+        }
     }
 }
